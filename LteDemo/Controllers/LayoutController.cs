e91afc9bad96c6c4205d391c4006bd123a40d7fb
liufei <liufei@123.com>
2017-07-05 17:27:04 +0800

    添加项目文件。

---
+++ LteDemo/Controllers/LayoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LteDemo.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: Layout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// from表单demo
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult From()
+        {
+            return View();
+        }
+    }
+}
---
+++ LteDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LteDemo.Startup))]
+namespace LteDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
